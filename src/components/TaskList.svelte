a669f0bf3adf844ab859bbf8595be9b75f801c2b
Dima Lipin <dimich3d@ya.ru>
2023-12-12 17:30:55 +0100

    editable

--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -3,16 +3,15 @@
 	import TaskListItem from './TaskListItem.svelte';
 
 	export let name = '';
-  export let items = [];
-
+	export let items = [];
 </script>
 
 <div class="flex-it w-80 rounded-xl border-2 border-blue-100 bg-sky-400 m-2 my-0">
 	<TaskListHeader {name} />
 	<div class="p-2">
 		{#each items as item (item.id)}
-      <TaskListItem task={item.text} />
-    {/each}
+			<TaskListItem task={item.text} />
+		{/each}
 	</div>
 	<button class="underline flex p-2"> + Add Task </button>
 </div>
--- src/components/TaskListItem.svelte
+++ src/components/TaskListItem.svelte
@@ -1,12 +1,15 @@
 <script>
 	import TrashBin from './TrashBin.svelte';
+	import Editable from './Editable.svelte';
 
-  export let task = '';
+	export let task = '';
 </script>
 
-<div class="flex-it flex-row border border-solid rounded-xl cursor-grab bg-sky-500 p-2 mb-2">
-	<div class="flex-1">{task}</div>
-	<div class="flex items-center hover:text-red-600">
-		<TrashBin />
-	</div>
-</div>
+<Editable>
+	<div class="flex-it flex-row border border-solid rounded-xl cursor-grab bg-sky-500 p-2 mb-2">
+		<div class="flex-1">{task}</div>
+		<div class="flex items-center hover:text-red-600">
+			<TrashBin />
+		</div>
+	</div></Editable
+>
