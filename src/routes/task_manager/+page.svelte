78cb35343dd6f5870ca342d3a88769529ae2304d
Dima Lipin <dimich3d@ya.ru>
2023-12-13 16:20:43 +0100

    move dummy data to store

--- src/routes/task_manager/+page.svelte
+++ src/routes/task_manager/+page.svelte
@@ -2,49 +2,15 @@
 	import AddListButton from "../../components/AddListButton.svelte";
 	import TaskList from "../../components/TaskList.svelte";
 
-	const taskList = [
-		{
-			id: "l-1",
-			text: "List 1",
-			items: [
-				{ id: "t-1", text: "Task 1" },
-				{ id: "t-2", text: "Task 2" },
-				{ id: "t-3", text: "Task 3" }
-			]
-		},
-		{
-			id: "l-2",
-			text: "List 2",
-			items: [
-				{ id: "t-4", text: "Task 4" },
-				{ id: "t-5", text: "Task 5" },
-				{ id: "t-6", text: "Task 6" }
-			]
-		},
-		{
-			id: "l-3",
-			text: "List 3",
-			items: [
-				{ id: "t-7", text: "Task 7" },
-				{ id: "t-8", text: "Task 8" },
-				{ id: "t-9", text: "Task 9" }
-			]
-		}
-	];
-
 	import { tasksStore } from "../../stores/tasks.js";
 
 </script>
 
 <div class="p-10 h-full">
-	<div class="text-white">
-		{JSON.stringify($tasksStore)}
-	</div>
-
 	<div class="text-white text-4xl mb-4">Some List</div>
 	<AddListButton />
 	<div class="flex-it flex-row">
-		{#each taskList as item (item.id)}
+		{#each $tasksStore as item (item.id)}
 			<TaskList
 				name={item.text}
 				items={item.items}
--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -1,22 +1,38 @@
 import { writable } from 'svelte/store';
 
 function createTasks() {
-  let id = 0;
-  const store = writable([],
-    () => {
-      setInterval(() => {
-        store.update(tasks => {
-          return [...tasks, id++];
-        })
-      }, 1000);
-    });
+  const taskList = [
+    {
+      id: "l-1",
+      text: "List 1",
+      items: [
+        { id: "t-1", text: "Task 1" },
+        { id: "t-2", text: "Task 2" },
+        { id: "t-3", text: "Task 3" }
+      ]
+    },
+    {
+      id: "l-2",
+      text: "List 2",
+      items: [
+        { id: "t-4", text: "Task 4" },
+        { id: "t-5", text: "Task 5" },
+        { id: "t-6", text: "Task 6" }
+      ]
+    },
+    {
+      id: "l-3",
+      text: "List 3",
+      items: [
+        { id: "t-7", text: "Task 7" },
+        { id: "t-8", text: "Task 8" },
+        { id: "t-9", text: "Task 9" }
+      ]
+    }
+  ];
+
+  const store = writable(taskList)
   return store;
-  // return {
-  //   subscribe,
-  //   add: (task) => update(tasks => [...tasks, task]),
-  //   remove: (task) => update(tasks => tasks.filter(t => t.id !== task.id)),
-  //   reset: () => set([])
-  // };
 }
 
 export const tasksStore = createTasks();
