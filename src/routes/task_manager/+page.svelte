e1660d84ad026d229de29bd9847a65fc0debbb99
Dmitrii Lipin <dmitrii.lipin@projectx.ai>
2023-12-14 15:02:27 +0100

    preload fn

--- src/routes/task_manager/+page.svelte
+++ src/routes/task_manager/+page.svelte
@@ -3,10 +3,12 @@
 	import TaskList from "../../components/TaskList.svelte";
 
 	import { tasksStore } from "../../stores/tasks.js";
+
+  export let data;
 </script>
 
 <div class="p-10 h-full flex-it">
-	<div class="text-white text-4xl mb-4">Some List</div>
+	<div class="text-white text-4xl mb-4">{data.appName}</div>
 	<AddListButton />
 	<div class="flex-it h-full flex-1">
 		<div class="flex-it flex-row rounded-xl h-full">
