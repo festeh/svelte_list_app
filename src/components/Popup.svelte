7168e9c0284611249a64e54cab4e9415fbc6b6b6
Dima Lipin <dimich3d@ya.ru>
2023-12-16 20:44:46 +0100

    popup

---
+++ src/components/Popup.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { beforeUpdate, onMount, afterUpdate, onDestroy } from "svelte";
+	let isOpen = false;
+
+	onMount(() => {
+		addEventListener("click", () => {
+			isOpen = false;
+		});
+		return () => {
+			removeEventListener("click", () => {
+				isOpen = false;
+			});
+		};
+	});
+</script>
+
+{#if isOpen}
+	<div
+		class="
+  flex flex-row justify-between w-full items-center p-2 rounded-2xl
+  hover:bg-gray-800"
+	>
+		Logout
+	</div>
+{:else}
+	<button
+		class=""
+		on:click|stopPropagation={() => (isOpen = true)}
+	>
+		<slot />
+	</button>
+{/if}
